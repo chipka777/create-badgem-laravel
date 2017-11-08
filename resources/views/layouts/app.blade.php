00f76a89179167b6307d2430492e8df44e273dcd
Igor <sg.i.apunevich@gmail.com>
2017-11-08 16:57:11 +0200

    test vuejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,24 +2,21 @@
 <html>
     <head>
         <title>Badgem Members Home</title>
-
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-
-
-
 
         <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js" type="text/javascript"></script>
-        <script src="js/custom.js" type="text/javascript"></script>
-        <script src="js/canvas.js" type="text/javascript"></script>
         <script src="https://use.fontawesome.com/03eca4d1ce.js"></script>
+        <script src="{{ asset('js/custom.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/canvas.js') }}" type="text/javascript"></script>
 
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body>
         @yield('content')
-        <script src="js/main.js"></script>
+        <script src="{{ asset('js/main.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 
 </html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,67 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
+    <div id="app">
+        <div class="main">
+            <div class="menu-element menu-category file-btn-search"></div>
+            <div class="menu-element menu-left prev" page-value="1"></div>
+            <div class="menu-element menu-instagram"  onclick="getInsta()"></div>
+            <div class="menu-element menu-members" onclick="showMain('login')"></div>
+            <div class="menu-element menu-right next" page-value="1"></div>
+            <div class="menu-element menu-bitcoin" onclick="showMain('bitcoin')"></div>
 
-    <div class="main">
-        <div class="menu-element menu-category file-btn-search"></div>
-        <div class="menu-element menu-left prev" page-value="1"></div>
-        <div class="menu-element menu-instagram"  onclick="getInsta()"></div>
-        <div class="menu-element menu-members" onclick="showMain('login')"></div>
-        <div class="menu-element menu-right next" page-value="1"></div>
-        <div class="menu-element menu-bitcoin" onclick="showMain('bitcoin')"></div>
+            <div class="menu-center">
+                <div id="menu-text" class="main-text" data-prev='0'>
+                    <h2>Welcome!</h2>
+                </div>
 
-        <div class="menu-center">
-            <div id="menu-text" class="main-text" data-prev='0'>
-                <h2>Welcome!</h2>
-            </div>
+                <div id="drop" class="drop-text" data-prev='0'>
+                    <h2>Drop me here!</h2>
+                </div>
 
-            <div id="drop" class="drop-text" data-prev='0'>
-                <h2>Drop me here!</h2>
-            </div>
+                <div id="cat-search" class="category-search" data-prev='0'>
+                    <h3>Category Search !</h3>
+                    <select name="searchCate" onchange="searchCate(this.value)">
+                        <option value="">Select Category</option>
+                        @foreach($categories as $category)
+                            <option value='{{ $category->name }}'>{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
 
-            <div id="cat-search" class="category-search" data-prev='0'>
-                <h3>Category Search !</h3>
-                <select name="searchCate" onchange="searchCate(this.value)">
-                    <option value="">Select Category</option>
-                    @foreach($categories as $category)
-                        <option value='{{ $category->name }}'>{{ $category->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+                <div id="login" class="login-menu" data-prev='0'>
+                    <h3>Authorization</h3>
+                    <input type="email" placeholder="Email">
+                    <input type="password" placeholder="Password">
+                    <input type="submit" value="Login">
+                </div>
 
-            <div id="login" class="login-menu" data-prev='0'>
-                <h3>Authorization</h3>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <input type="submit" value="Login">
-            </div>
-
-            <div id="bitcoin" class="bitcoin-menu" data-prev='0'>
-                <h2>Bitcoin Section</h2>
-            </div>
-        </div>
-    </div><!-- END MAIN -->
-    <div class="main-canvas">
-        <div class="scale-line">
-            <div class="scale-back">
-                <div id="canvas-line" class="scale-line-front" ondragstart="return false;" >
-                    <div id="canvas-scale" onmousedown="canvasScale(event, $(this))" class="scale-btn"></div>
+                <div id="bitcoin" class="bitcoin-menu" data-prev='0'>
+                    <h2>Bitcoin Section</h2>
                 </div>
             </div>
-        </div>
-        <div class="canvas" ondragstart="return false;" onmousedown="hideLatest()"></div>
-        <div class="canvas-btn">
-            <div class="canvas-btn-home" onclick="canvasHide()" ></div>
-            <div class="canvas-btn-scale"></div>
-            <div class="canvas-btn-print" onclick="savePDF()"></div>
-        </div>
-    </div>
-    <div class="panels">
-        @foreach($images as $key => $image)
-            <div id='img-{{ $image->id }}' class='panel pos{{ $key+1 }} canva-img'>
-                <img onmousedown='panelImg(event, $(this))' src='upload/{{ $image->name }}' />
+        </div><!-- END MAIN -->
+        <div class="main-canvas">
+            <div class="scale-line">
+                <div class="scale-back">
+                    <div id="canvas-line" class="scale-line-front" ondragstart="return false;" >
+                        <div id="canvas-scale" onmousedown="canvasScale(event, $(this))" class="scale-btn"></div>
+                    </div>
+                </div>
             </div>
-        @endforeach
+            <div class="canvas" ondragstart="return false;" onmousedown="hideLatest()"></div>
+            <div class="canvas-btn">
+                <div class="canvas-btn-home" onclick="canvasHide()" ></div>
+                <div class="canvas-btn-scale"></div>
+                <div class="canvas-btn-print" onclick="savePDF()"></div>
+            </div>
+        </div>
+        <div class="panels">
+            @foreach($images as $key => $image)
+                <div id='img-{{ $image->id }}' class='panel pos{{ $key+1 }} canva-img'>
+                    <img onmousedown='panelImg(event, $(this))' src='upload/{{ $image->name }}' />
+                </div>
+            @endforeach
+        </div>
+        <example></example>
     </div>
-
 @endsection
